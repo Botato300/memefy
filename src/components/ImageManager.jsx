89c8da4a0ed82c67ac83f32c145ee5256ed26b6b
Botato300 <bigvotati@gmail.com>
2025-05-23 02:33:56 -0300

    crea un archivo de css para ImageManager.jsx

--- src/components/ImageManager.jsx
+++ src/components/ImageManager.jsx
@@ -3,11 +3,23 @@ import { useState } from "react";
 import DropZone from "./DropZone.jsx";
 import ImageEditor from "./ImageEditor.jsx";
 
+import s from "./ImageManager.module.css";
+
 export default function ImageManager() {
     const [file, setFile] = useState(null);
 
     if (file)
-        return <ImageEditor imgFile={file} />;
+        return (
+            <>
+                <ImageEditor imgFile={file} />
+                <span className={s.title}>Escríbele un texto y ya lo tienes...</span>
+            </>
+        );
 
-    return <DropZone setFile={setFile} />;
+    return (
+        <>
+            <DropZone setFile={setFile} />
+            <span className={s.title}>Elige una imagen para memeficar</span>
+        </>
+    );
 }
