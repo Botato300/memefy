353736077f78a97b72ce27d1076a9ec261d79026
Botato300 <bigvotati@gmail.com>
2025-06-18 23:53:37 -0300

    feat: add a transition to display the meme count

--- src/frontend/components/MemeCount.jsx
+++ src/frontend/components/MemeCount.jsx
@@ -8,17 +8,24 @@ import s from "./MemeCount.module.css";
 
 export default function MemeCount() {
     const [count, setCount] = useState(0);
+    const [isSuccess, setIsSuccess] = useState(false);
+
     const { file } = useContext(FileContext);
 
     useEffect(() => {
         getMemeCount()
-            .then(count => setCount(count))
+            .then(count => {
+                setCount(count);
+                setIsSuccess(true);
+            })
             .catch(() => setCount(null));
     }, []);
 
     useEffect(() => {
+        if (!file) return;
+
         setCount(count + 1);
     }, [file]);
 
-    return count === null ? null : <span className={s.text_count}>Total de memes creados: {count}</span>;
+    return count === null ? null : <span className={`${s.text_count} ${isSuccess ? s.show : ""}`}>Total de memes creados: {count}</span>;
 }
